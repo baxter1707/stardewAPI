4181a6dd7608df438c3389f308869bc8fc5a9803
Michael Baxter <michael_steven.baxter@yahoo.com>
2018-07-09 10:22:44 -0500

    this is my second try

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.get('*', (req, res) => res.status(200).send({
-//   message: 'Welcome to the beginning of nothingness.',
-// }));
+app.get('/', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness.',
+}));
 
 
 
